6ab9ed1041822115e506347432f3172770815180
Miko <morita.mcp@gmail.com>
2024-12-17 15:41:09 +0800

    primitive data type

---
+++ 6 - JavaScript/2-primitive-data-type.js
@@ -0,0 +1,33 @@
+// Primitive Data Types
+// String: textual data enlosed in '' or ""
+let firstName = "Miko";
+console.log("Firstname:", firstName);
+
+// typeof: check the data type of a certain variable
+console.log("Data Type:", typeof firstName);
+
+// Number: numerical value (30, -30, 3.14, -3.14)
+let age = 30;
+console.log("Age:", age);
+console.log("Data Type:", typeof age);
+
+// Booleans: either true or false
+let isStudent = true;
+console.log("Is Student:", isStudent);
+console.log("Data Type:", typeof isStudent);
+
+// Null: Intentional absence of a value
+let car = null;
+console.log("Car:", car);
+console.log("Data Type:", typeof car);
+car = "Ferrari";
+console.log("Car:", car);
+console.log("Data Type:", typeof car);
+
+// Undefined: Unintentional absence of a value
+let city;
+console.log("City:", city);
+console.log("Data Type:", typeof city);
+city = "Paris";
+console.log("City:", city);
+console.log("Data Type:", typeof city);
