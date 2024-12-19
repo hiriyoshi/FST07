a14f2e8ebffbcea8c5221d284fdcf2fb1237ca17
Miko <morita.mcp@gmail.com>
2024-12-19 14:26:29 +0800

    app.js

---
+++ 8 - Task Manager App/app.js
@@ -0,0 +1,5 @@
+// 1. Select DOM Elements
+const todoInput = document.querySelector(".todo-input");
+const todoButton = document.querySelector(".todo-button");
+const filterTodo = document.querySelector(".filter-todo");
+const todoList = document.querySelector(".todo-list");
